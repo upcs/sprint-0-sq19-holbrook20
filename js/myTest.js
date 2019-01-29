f6a6a2676532632a16da759d3c3a4a98dd955ee3
holbrook20 <35584025+holbrook20@users.noreply.github.com>
2019-01-28 21:08:41 -0800

    Update myTest.js

--- js/myTest.js
+++ js/myTest.js
@@ -1,10 +1,5 @@
 const sum = require('./functions');
 
-test('adds 5 + 5 to equal 7', () => {
-  expect(sum(5,5)).toBe(10);
-  
-  });
-  
-test('Difference between sum and number is the other number', () => {
-  expect(difference(sum(5,5),5).toBe(5);
-  });
+test('adds 5 + 5 to equal 10', () => {
+  expect(sum(5, 5)).toBe(10);
+});
