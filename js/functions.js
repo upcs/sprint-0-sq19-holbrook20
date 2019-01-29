2d68de0559f39d5aa0d70619795c263839020750
holbrook20 <holbrook20@up.edu>
2019-01-29 09:08:25 -0800

    Updated the test files
    Functions didn't have an export for the testing. Also changed the name of  myTest() to functions.test()

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,7 @@ var b = 5;
 function sum(a, b){
   return a + b;
 }
+
 document.write(sum(a,b));
+
+module.exports = sum;
---
+++ js/functions.test.js
@@ -0,0 +1,5 @@
+const sum = require('./functions');
+
+test('adds 5 + 5 to equal 10', () => {
+  expect(sum(5, 5)).toBe(10);
+});
